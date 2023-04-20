2f7f03891e4dd922e934ca48f0555ea5bd03635c
Nail Uspanov <uspanov@aeroidea.ru>
2023-04-20 22:35:17 +0400

    new major

--- tracer.go
+++ tracer.go
@@ -1,69 +1,153 @@
 package tracer
 
 import (
-	"fmt"
-	"github.com/gin-gonic/gin"
+	"bytes"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/ext"
-	zerologger "github.com/rs/zerolog/log"
-	"github.com/uber/jaeger-client-go"
-	jaegercfg "github.com/uber/jaeger-client-go/config"
-	jaegerlog "github.com/uber/jaeger-client-go/log"
-	"github.com/uber/jaeger-client-go/zipkin"
-	"github.com/uber/jaeger-lib/metrics"
-	"io"
-	"net/http"
+	fields "github.com/opentracing/opentracing-go/log"
+	"runtime/debug"
+	"sync"
 )
 
-func SetJaegerTracer(traceHeader string) (opentracing.Tracer, io.Closer, error) {
-	cfg, err := jaegercfg.FromEnv()
-	if err != nil {
-		zerologger.Error().Msg(fmt.Sprintf("Could not parse Jaeger env vars: %s", err.Error()))
-	}
-	customHeaders := &jaeger.HeadersConfig{
-		TraceContextHeaderName: traceHeader,
-	}
-	cfg.Headers = customHeaders
-	jLogger := jaegerlog.StdLogger
-	jMetricsFactory := metrics.NullFactory
-	// Zipkin shares span ID between client and server spans; it must be enabled via the following option.
-	zipkinPropagator := zipkin.NewZipkinB3HTTPHeaderPropagator()
-	tracer, closer, err := cfg.NewTracer(
-		jaegercfg.Logger(jLogger),
-		jaegercfg.Metrics(jMetricsFactory),
-		jaegercfg.Injector(opentracing.HTTPHeaders, zipkinPropagator),
-		jaegercfg.Extractor(opentracing.HTTPHeaders, zipkinPropagator),
-	)
-	if err != nil {
-		zerologger.Error().Msg(fmt.Sprintf("Could not create Jaeger traces: %s", err.Error()))
-	}
-	return tracer, closer, err
+var (
+	scopeManager   = &ScopeManager{}
+	lock           = sync.RWMutex{}
+	tracerDisabled = false
+)
+
+func SetTracerDisabled(disabled bool) {
+	tracerDisabled = disabled
 }
-func OpenTracingMiddleware() gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var requestSpan opentracing.Span
-		spCtx, err := opentracing.GlobalTracer().Extract(opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(c.Request.Header))
-		if err != nil {
-			requestSpan = opentracing.StartSpan(c.Request.URL.Path)
-			defer requestSpan.Finish()
-		} else {
-			requestSpan = opentracing.StartSpan(
-				c.Request.URL.Path,
-				opentracing.ChildOf(spCtx),
-				opentracing.Tag{Key: string(ext.Component), Value: "HTTP"},
-				ext.SpanKindRPCServer,
-			)
-			defer requestSpan.Finish()
+
+type TracerInterface interface {
+	GetScope() *Scope
+	Close()
+	SetTag(key string, value interface{})
+	SetTags(list map[string]interface{})
+	LogMessage(message string)
+	Log(key, message string)
+	LogData(key string, data interface{})
+	LogError(err interface{})
+}
+
+func NewTracer(tracerName string, opts ...opentracing.StartSpanOption) TracerInterface {
+	if tracerDisabled {
+		return &NullTracer{}
+	}
+	lock.Lock()
+	defer lock.Unlock()
+	gr := string(bytes.Fields(debug.Stack())[1])
+	preparedOpts, async := initTracer(gr, opts...)
+	activeSpan := opentracing.StartSpan(tracerName, preparedOpts...)
+	scope := scopeManager.Activate(activeSpan)
+	scope.Async = async
+	scope.Gr = gr
+	return &Tracer{scope: scope}
+}
+
+func initTracer(gr string, opts ...opentracing.StartSpanOption) ([]opentracing.StartSpanOption, bool) {
+	scope := scopeManager.GetActive()
+	async := false
+	asyncSet := false
+	parentSet := false
+	for _, op := range opts {
+		switch v := op.(type) {
+		case ParentSpan:
+			scope = v.Parent
+			parentSet = true
+		case AsyncSpan:
+			async = v.Async
+			asyncSet = true
 		}
-		c.Set("tracing-context", requestSpan)
-		c.Next()
+	}
+	if scope != nil {
+		if !asyncSet {
+			async = scope.Async
+		}
+
+		var syncScope *Scope
+		if parentSet {
+			syncScope = scope
+		} else {
+			syncScope = scope.GetParent(gr)
+		}
+		if syncScope != nil {
+			opts = append(opts, opentracing.ChildOf(syncScope.GetSpan().Context()))
+		}
+	}
+	return opts, async
+}
+
+type Tracer struct {
+	scope    *Scope
+	hasError bool
+}
+
+func (t *Tracer) GetScope() *Scope {
+	return t.scope
+}
+
+func (t *Tracer) Close() {
+	t.scope.Close()
+}
+
+func (t *Tracer) SetTag(key string, value interface{}) {
+	t.scope.GetSpan().SetTag(key, value)
+}
+
+func (t *Tracer) SetTags(list map[string]interface{}) {
+	for key, value := range list {
+		t.scope.GetSpan().SetTag(key, value)
 	}
 }
 
-func AddTraceToRequest(c *gin.Context, req *http.Request) {
-	tracer := opentracing.GlobalTracer()
-	if cspan, ok := c.Get("tracing-context"); ok {
-		tracer.Inject(cspan.(opentracing.Span).Context(), opentracing.HTTPHeaders, opentracing.HTTPHeadersCarrier(req.Header))
-	}
+func (t *Tracer) LogMessage(message string) {
+	t.scope.GetSpan().LogFields(fields.String("message", message))
+}
 
+func (t *Tracer) Log(key, message string) {
+	t.scope.GetSpan().LogFields(fields.String(key, message))
+}
+
+func (t *Tracer) LogData(key string, data interface{}) {
+	t.scope.GetSpan().LogFields(fields.Object(key, data))
+}
+
+func (t *Tracer) LogError(err interface{}) {
+	if t.hasError != true {
+		ext.Error.Set(t.scope.GetSpan(), true)
+		t.hasError = true
+	}
+	t.scope.GetSpan().LogFields(
+		fields.Object("error.object", err),
+		fields.Event("error"),
+	)
+}
+
+type NullTracer struct {
+}
+
+func (t *NullTracer) GetScope() *Scope {
+	return nil
+}
+
+func (t *NullTracer) Close() {
+}
+
+func (t *NullTracer) SetTag(key string, value interface{}) {
+}
+
+func (t *NullTracer) SetTags(list map[string]interface{}) {
+}
+
+func (t *NullTracer) LogMessage(message string) {
+}
+
+func (t *NullTracer) Log(key, message string) {
+}
+
+func (t *NullTracer) LogData(key string, data interface{}) {
+}
+
+func (t *NullTracer) LogError(err interface{}) {
 }
